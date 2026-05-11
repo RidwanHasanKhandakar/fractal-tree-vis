8a324cdc45a4057a741b79038b35ab753e0538dd
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-05-11 10:46:07 +0600

    made a slider

--- sketch.js
+++ sketch.js
@@ -1,8 +1,12 @@
+var  angle = PI/4;
+var slider;
 function setup(){
     createCanvas(400,400);
+    slider = createSlider(0,TWO_PI,PI/4,0.01);
 }
 function draw(){
     background(51);
+    angle=slider.value();
     stroke(255);
     translate(200,height);
     branch(100);
@@ -10,6 +14,9 @@ function draw(){
 function branch(len){
     line(0,0,0,-len);
     translate(0,-len);
-    rotate(PI/4);
-    line(0,0,0,-len);
+    rotate(angle);
+    if(len>4){
+        branch(len*0.67);
+    }
+    //line(0,0,0,-len*0.67);
 }
